e266fe0b20b7c6d24a8be6783160c8d9c75fd184
alexeq <alexeq@yahoo.com>
2015-03-20 02:16:26 +0300

    initial commit

---
+++ server.R
@@ -0,0 +1,59 @@
+library(shiny)
+library(ggplot2)
+
+# Body Water constant
+BW <- function(sex) {
+    if (sex == "male") 0.58 else 0.49
+}
+
+# Metabolism 
+MR <- function(sex) {
+    if (sex == "male") c(0.013, 0.015, 0.017) else c(0.014, 0.017, 0.021)
+}
+
+# Estimated blood alcohol concentration
+EBAC <- function(units, sex, weight, shots, hours) {
+    wt <- if (units == "kg") weight else kilos(weight)
+    ebac = (0.806 * shots * 1.2) / (BW(sex) * wt) - (MR(sex) * hours)
+    ifelse(ebac > 0, ebac, 0)
+}
+
+# Convert pounds to kilos
+kilos <- function(pounds) pounds / 2.2046
+
+shinyServer(function(input, output) { 
+    # Calculate BAC level
+    bac <- reactive({
+        round(EBAC(input$units, input$sex, input$weight, input$shots, input$hours)[2], 4)
+    })
+    
+    # Show formatted BAC
+    output$bac <- renderText({
+        paste0(bac(), "%")
+    })
+    
+    # Show advice based on BAC
+    output$summary <- renderText({
+        if (bac() == 0) "Congratulations! You are good to go!" 
+        else if (bac() <= 0.02) "You are allowed to drive in some countries, but be careful!"
+        else "Be safe and call taxi!"
+    })
+    
+    # Render plot
+    output$plot <- renderPlot({
+        hrs <- seq(0, 24, 0.5)
+        bac <- sapply(hrs, function(x) EBAC(input$units, input$sex, input$weight, input$shots, x))
+        df <- data.frame(Hours = hrs, t(bac))
+        dfNow <- subset(df, Hours == input$hours) 
+        ggplot(df, aes(x = Hours, y = X2)) + 
+            geom_line() + 
+            geom_ribbon(aes(x = Hours, ymin = X1, ymax = X3, linetype = NA), alpha = 0.2) +
+            xlab("Hours") +
+            ylab("BAC (%)") +
+            geom_point(data = dfNow, size = 3, color = "red") +
+            geom_text(data = dfNow, label = "Your level", vjust = -1, hjust = 0) +
+            geom_ribbon(aes(x = Hours, ymin = 0, ymax = 0.02, linetype = NA, fill = "green"), alpha = 0.2) +
+            scale_fill_manual(values=c("green"="green")) +
+            theme(legend.position = "none")
+    })    
+})
---
+++ ui.R
@@ -0,0 +1,57 @@
+library(shiny)
+
+shinyUI(fluidPage(
+    
+    # Application title
+    titlePanel("Blood Alcohol Concentration (BAC) Calculator"),
+    
+    # Description panel
+    withTags({
+        div(class="header", 
+            span("Had couple of drinks and thinking about driving home?"),
+            span("Check out how little is needed to change alcohol level in your blood!"),
+            span("Read"),
+            a(href="http://en.wikipedia.org/wiki/Blood_alcohol_content", "Wikipedia"),
+            span("about more information on the subject.")
+        )        
+    }),
+    helpText(
+        span('This is a project for "Developing Data Products" class at Coursera.'),
+        span('Sources are available on'),
+        a(href="http://github.com", "GitHub")
+    ),
+    
+    # Sidebar with input controls
+    sidebarLayout(
+        sidebarPanel(            
+            selectInput('units', 'Select Measurement Units:', 
+                c("Kilograms" = "kg", "Pounds" = "lb")),
+            helpText("Play with controls to see results:"),            
+            
+            radioButtons("sex", "Gender:",
+                c("Male" = "male", "Female" = "female")),        
+            numericInput("weight", "Body weight:", 
+                value = 65, min = 40, max = 300, step = 1),
+            sliderInput("shots", "Number of shots:", 
+                value = 1, min = 1, max = 30),
+            helpText("(One beer can or a glass of wine are equivalent to 1 shot)"),
+            sliderInput("hours", "Hours since starting drinking:", 
+                value = 1, min = 1, max = 24)          
+        ),
+        
+        # Calculation results
+        mainPanel(
+            strong(
+                span("Your estimated BAC level is:"), 
+                textOutput("bac", inline = TRUE)),
+            verbatimTextOutput("summary"),
+            helpText(
+                strong("Warning!"),
+                span("Actual result may differ due to different metabolism and other factors!")
+            ),
+            span('Plot below shows how BAC degrades with time. 
+                Your level is shown as a red dot and green area denotes "safe level".'),
+            plotOutput("plot")
+        )
+    )    
+))
